f7e028a82c9767fd297cb9e9f96471d9e77266c1
danding <danding@ixiaopu.com>
2018-12-28 19:08:13 +0800

    Modify: 地图获取地理位置

--- src/pages/index/index.vue
+++ src/pages/index/index.vue
@@ -5,13 +5,35 @@
 </template>
 
 <script>
+import wxSync from '@/utils/wxApiSync'
+
 export default {
   onShareAppMessage() {
     return {
       title: 'ta就在你走的这条小路上',
       path: '/pages/index/main'
     }
-  }
+  },
+  async beforeMount() {
+    // wx.authorize('scope.userLocation')
+    const settings = await wxSync({ api: 'getSetting' })
+    if (settings.authSetting['scope.userLocation']) {
+      const location = await wxSync({ api: 'getLocation' })
+    } else {
+      try {
+        const location = await wxSync({ 
+          api: 'authorize',
+          params: { scope: 'scope.userLocation' 
+        }})
+      } catch () {
+        wx.showToast({
+          title: '无法获取位置，将无法体验功能哦',
+          icon: 'none',
+          duration: 2000
+        });
+      }
+    }
+  },
 };
 </script>
 
