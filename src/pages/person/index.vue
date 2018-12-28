0e805c5a02ef7555c9b5ee2d71060ba241eb1912
danding <danding@ixiaopu.com>
2018-12-28 18:46:19 +0800

    Modify: 增加了初始化体验

--- src/pages/person/index.vue
+++ src/pages/person/index.vue
@@ -1,6 +1,7 @@
 <template>
   <div class="container">
-    <view v-show="nickName" class="profile-wrapper">
+    <view v-if="init">
+      <view v-show="nickName" class="profile-wrapper">
       <image
         class="avatar"
         :src="avatarUrl"
@@ -8,38 +9,43 @@
       <view>
         <text>{{nickName}}</text>
       </view>
-    </view>
-    <view class="weui-cells__title">描述下自己的穿着打扮</view>
-      <view class="weui-cells weui-cells_after-title">
-        <view class="weui-cell weui-cell_input">
-          <view class="weui-cell__hd">
-            <view class="weui-label">衣服</view>
-          </view> 
-          <view class="weui-cell__bd">
-            <input v-model="clothsDesc" @change class="weui-input" placeholder="颜色、款式等描述"/>
-          </view>
-        </view>
-        <view class="weui-cell weui-cell_input">
-          <view class="weui-cell__hd">
-              <view class="weui-label">裤子</view>
-          </view>
-          <view class="weui-cell__bd">
-              <input v-model="trousersDesc" class="weui-input" placeholder="颜色、款式等描述"/>
-          </view>
-        </view>
-        <view class="weui-cell weui-cell_input">
-          <view class="weui-cell__hd">
-              <view class="weui-label">鞋子</view>
-          </view>
-          <view class="weui-cell__bd">
-              <input v-model="shoesDesc" class="weui-input" placeholder="颜色、款式等描述"/>
-          </view>
-        </view>
       </view>
-    <view class="save-wrapper">
-      <button @click="updateInfo" class="weui-btn" type="primary">更新</button>
+      <view class="weui-cells__title">描述下自己的穿着打扮</view>
+        <view class="weui-cells weui-cells_after-title">
+          <view class="weui-cell weui-cell_input">
+            <view class="weui-cell__hd">
+              <view class="weui-label">衣服</view>
+            </view> 
+            <view class="weui-cell__bd">
+              <input v-model="clothsDesc" @change class="weui-input" placeholder="颜色、款式等描述"/>
+            </view>
+          </view>
+          <view class="weui-cell weui-cell_input">
+            <view class="weui-cell__hd">
+                <view class="weui-label">裤子</view>
+            </view>
+            <view class="weui-cell__bd">
+                <input v-model="trousersDesc" class="weui-input" placeholder="颜色、款式等描述"/>
+            </view>
+          </view>
+          <view class="weui-cell weui-cell_input">
+            <view class="weui-cell__hd">
+                <view class="weui-label">鞋子</view>
+            </view>
+            <view class="weui-cell__bd">
+                <input v-model="shoesDesc" class="weui-input" placeholder="颜色、款式等描述"/>
+            </view>
+          </view>
+        </view>
+      <view class="save-wrapper">
+        <button @click="updateInfo" class="weui-btn" type="primary">更新</button>
+      </view>
+      <user-info-pane :show="showAuthorize" @hidePane="hideAuthorize" @getUserInfoSuc="authorizedSuc" />
     </view>
-    <user-info-pane :show="showAuthorize" @hidePane="hideAuthorize" @getUserInfoSuc="authorizedSuc" />
+    <view v-else class="weui-loadmore loading-wrapper">
+        <view class="weui-loading"></view>
+        <view class="weui-loadmore__tips loading-tip">正在加载</view>
+    </view>
   </div>
 </template>
 
@@ -56,7 +62,8 @@ export default {
       nickName: '路人甲',
       trousersDesc: '',
       shoesDesc: '',
-      clothsDesc: ''
+      clothsDesc: '',
+      init: false,
     }
   },
   components: {
@@ -88,15 +95,25 @@ export default {
     },
     async setUserInfo() {
       const userInfo = await wxCloudSync('getUserInfo');
-      const { nickName, avatarUrl } = userInfo.result.data;
+      const {
+        nickName,
+        avatarUrl,
+        clothsDesc,
+        trousersDesc,
+        shoesDesc
+      } = userInfo.result.data;
       if (nickName) {
         this.nickName = nickName;
         this.avatarUrl = avatarUrl;
+        this.shoesDesc = shoesDesc;
+        this.trousersDesc = trousersDesc;
+        this.clothsDesc = clothsDesc;
       }
     }
   },
   async beforeMount() {
     await this.setUserInfo()
+    this.init = true;
   }
 };
 </script>
@@ -116,5 +133,13 @@ export default {
 
   .save-wrapper {
     padding: 100rpx 30rpx 10rpx;
+  }
+
+  .loading-wrapper {
+    padding-top: 150rpx;
+  }
+
+  .loading-tip {
+    color: #999999;
   }
 </style>
